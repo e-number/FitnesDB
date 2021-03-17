2ea35dbc5d9a8c9c73c30465f34b7b53ba41ef85
e-number <eggnom@yandex.ru>
2021-03-17 22:18:48 +0300

    AddMemberActivity variables

--- app/src/main/java/com/example/fitnesdb/AddMemberActivity.java
+++ app/src/main/java/com/example/fitnesdb/AddMemberActivity.java
@@ -3,12 +3,25 @@ package com.example.fitnesdb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddMemberActivity extends AppCompatActivity {
+
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+    private EditText groupEditText;
+    private Spinner genderSpiner;
+    private int gender = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_member);
+
+        firstNameEditText = findViewById(R.id.firstNameEditText);
+        lastNameEditText = findViewById(R.id.lastNameEditText);
+        groupEditText = findViewById(R.id.groupEditText);
+        genderSpiner = findViewById(R.id.genderSpiner);
     }
 }
