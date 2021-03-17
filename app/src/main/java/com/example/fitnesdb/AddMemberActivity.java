fc603ccdfb91ca65db0064d8de4d454eb5005c1a
e-number <eggnom@yandex.ru>
2021-03-17 17:05:45 +0300

    AddMemberActivity

---
+++ app/src/main/java/com/example/fitnesdb/AddMemberActivity.java
@@ -0,0 +1,14 @@
+package com.example.fitnesdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMemberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_member);
+    }
+}
