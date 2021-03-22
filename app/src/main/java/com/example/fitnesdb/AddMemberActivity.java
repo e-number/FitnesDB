f9c5ba5c2c74c4e49f97bb18d72728483922d508
e-number <eggnom@yandex.ru>
2021-03-22 10:56:48 +0300

    edit_member_menu

--- app/src/main/java/com/example/fitnesdb/AddMemberActivity.java
+++ app/src/main/java/com/example/fitnesdb/AddMemberActivity.java
@@ -1,8 +1,15 @@
 package com.example.fitnesdb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -38,5 +45,46 @@ public class AddMemberActivity extends AppCompatActivity {
         spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         genderSpiner.setAdapter(spinnerAdapter);
+
+        genderSpiner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedGender = (String) parent.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selectedGender)) {
+                    if (selectedGender.equals("Male")) {
+                        gender = 1;
+                    } else if (selectedGender.equals("Female")) {
+                        gender = 2;
+                    } else {
+                        gender = 0;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                gender = 0;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_member_menu, menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_member:
+                return  true;
+            case R.id.delete_member:
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
