08b5cfe7d73aa413658db1591b902a9f3f5db56a
e-number <eggnom@yandex.ru>
2021-06-06 10:24:33 +0300

    CursorLoader multithreading

--- app/src/main/java/com/example/fitnesdb/MainActivity.java
+++ app/src/main/java/com/example/fitnesdb/MainActivity.java
@@ -1,52 +1,58 @@
 package com.example.fitnesdb;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.fitnesdb.data.FitnessClubContract.MemberEntry;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    TextView dataTextView;
+    private static final int MEMBER_LOADER = 123;
+    MemberCursorAdapter memberCursorAdapter;
+    ListView dataListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dataTextView = findViewById(R.id.dataTextView);
+        dataListView = findViewById(R.id.dataListView);
 
         FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddMemberActivity.class);
-                startActivity(intent);
-            }
+                floatingActionButton.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, AddMemberActivity.class);
+            startActivity(intent);
         });
+
+        memberCursorAdapter = new MemberCursorAdapter(this, null, false);
+        dataListView.setAdapter(memberCursorAdapter);
+
+        getSupportLoaderManager().initLoader(MEMBER_LOADER, null, this);
     }
+
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        displayData();
-    }
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
 
-    private void displayData() {
         String[] projection = {
                 MemberEntry._ID,
                 MemberEntry.COLUMN_FIRST_NAME,
                 MemberEntry.COLUMN_LAST_NAME,
-                MemberEntry.COLUMN_GENDER,
                 MemberEntry.COLUMN_SPORT
         };
-        Cursor cursor =getContentResolver().query(
+        CursorLoader cursorLoader = new CursorLoader( this,
                 MemberEntry.CONTENT_URI,
                 projection,
                 null,
@@ -54,26 +60,20 @@ public class MainActivity extends AppCompatActivity {
                 null
         );
 
-        dataTextView.setText("All members\n\n");
-        dataTextView.append(MemberEntry._ID + " " + MemberEntry.COLUMN_FIRST_NAME + " " + MemberEntry.COLUMN_LAST_NAME + " " +
-                MemberEntry.COLUMN_GENDER + " " + MemberEntry.COLUMN_SPORT);
+        return cursorLoader;
+    }
 
-        int idColumnIndex = cursor.getColumnIndex(MemberEntry._ID);
-        int firstNameColumnIndex = cursor.getColumnIndex(MemberEntry.COLUMN_FIRST_NAME);
-        int lastNameColumnIndex = cursor.getColumnIndex(MemberEntry.COLUMN_LAST_NAME);
-        int genderColumnIndex = cursor.getColumnIndex(MemberEntry.COLUMN_GENDER);
-        int sportColumnIndex = cursor.getColumnIndex(MemberEntry.COLUMN_SPORT);
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
 
-        while (cursor.moveToNext()) {
-            int currentId = cursor.getInt(idColumnIndex);
-            String currentFirstName = cursor.getString(firstNameColumnIndex);
-            String currenLastName = cursor.getString(lastNameColumnIndex);
-            int currentGender = cursor.getInt(genderColumnIndex);
-            String currenSport = cursor.getString(sportColumnIndex);
+        memberCursorAdapter.swapCursor(cursor);
 
-            dataTextView.append("\n" + currentId + " " + currentFirstName + " " + currenLastName + " " +
-                    currentGender + " " + currenSport);
-        }
-        cursor.close();
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+        memberCursorAdapter.swapCursor(null);
+
     }
 }
--- app/src/main/java/com/example/fitnesdb/data/FitnessClubContentProvider.java
+++ app/src/main/java/com/example/fitnesdb/data/FitnessClubContentProvider.java
@@ -3,6 +3,7 @@ package com.example.fitnesdb.data;
 import android.content.ContentProvider;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -66,6 +67,27 @@ public class FitnessClubContentProvider extends ContentProvider {
     @Override
     public Uri insert(@NonNull Uri uri, ContentValues values) {
 
+        String firstName = values.getAsString(MemberEntry.COLUMN_FIRST_NAME);
+        if (firstName == null) {
+            throw new IllegalArgumentException("You have to input first name");
+        }
+
+        String lastName = values.getAsString(MemberEntry.COLUMN_LAST_NAME);
+        if (lastName == null) {
+            throw new IllegalArgumentException("You have to input last name");
+        }
+
+        Integer gender = values.getAsInteger(MemberEntry.COLUMN_GENDER);
+        if (gender == null || !(gender == MemberEntry.GENDER_UNKNOWN || gender == MemberEntry.GENDER_MALE ||
+                gender == MemberEntry.GENDER_FEMALE)) {
+            throw new IllegalArgumentException("You have to input correct gender");
+        }
+
+        String sport = values.getAsString(MemberEntry.COLUMN_SPORT);
+        if (sport == null) {
+            throw new IllegalArgumentException("You have to input correct data");
+        }
+
         SQLiteDatabase db = dbOpenHelper.getWritableDatabase();
 
         int match = uriMatcher.match(uri);
@@ -108,6 +130,35 @@ public class FitnessClubContentProvider extends ContentProvider {
 
     @Override
     public int update(@NonNull Uri uri, ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+
+        if (values.containsKey(MemberEntry.COLUMN_FIRST_NAME)) {
+            String firstName = values.getAsString(MemberEntry.COLUMN_FIRST_NAME);
+            if (firstName == null) {
+                throw new IllegalArgumentException("You have to input first name");
+            }
+        }
+
+        if (values.containsKey(MemberEntry.COLUMN_LAST_NAME)) {
+            String lastName = values.getAsString(MemberEntry.COLUMN_LAST_NAME);
+            if (lastName == null) {
+                throw new IllegalArgumentException("You have to input last name");
+            }
+        }
+
+        if (values.containsKey(MemberEntry.COLUMN_GENDER)) {
+            Integer gender = values.getAsInteger(MemberEntry.COLUMN_GENDER);
+            if (gender == null || !(gender == MemberEntry.GENDER_UNKNOWN || gender == MemberEntry.GENDER_MALE ||
+                    gender == MemberEntry.GENDER_FEMALE)) {
+                throw new IllegalArgumentException("You have to input correct gender");
+            }
+        }
+
+        if (values.containsKey(MemberEntry.COLUMN_SPORT)) {
+            String sport = values.getAsString(MemberEntry.COLUMN_SPORT);
+            if (sport == null) {
+                throw new IllegalArgumentException("You have to input correct data");
+            }
+        }
 
         SQLiteDatabase db = dbOpenHelper.getWritableDatabase();
 
